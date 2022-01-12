7eb7296d8984cc3809e5500fb88a5c455ec274a8
EricM <ericmadson67@hotmail.com>
2022-01-12 16:45:09 -0300

    (UPDATE) fetch API

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,13 +3,6 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
-
-const repository = {
-    name: "unform2",
-    description: "Forms in React",
-    link: "https://github.com/unform/unform"
-};
-
 export function RepositoryList () {
     const [repositories, setRepositories] = useState([]);
 
@@ -25,10 +18,9 @@ export function RepositoryList () {
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
             <ul>
-             <RepositoryItem repository={repository} />
-             <RepositoryItem repository={repository} />
-             <RepositoryItem repository={repository} />
-             <RepositoryItem repository={repository} />
+             {repositories.map(repository => {
+                 return <RepositoryItem key={repository.name} repository={repository} />
+             })}
             </ul>
         </section>
     );
