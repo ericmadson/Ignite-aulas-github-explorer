992f4a11481fb91e6c9748c295d6ef15637e9233
EricM <ericmadson67@hotmail.com>
2022-08-24 19:36:44 -0300

    teste

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,18 +1,24 @@
+import { useState } from "react";
+
 interface RepositoryItemProps {
-    repository: {
-        name: string,
-        description: string,
-        html_url: string,
-    },
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  };
 }
 
-export function RepositoryItem (props: RepositoryItemProps) {
-    return (
-        <li>
-            <strong>{props.repository.name}</strong>
-            <p>{props.repository.description}</p>
-                    
-            <a target="_blank" href={props.repository.html_url}>Acessar repositório</a>
-        </li>
-    );
-};
+export function RepositoryItem(props: RepositoryItemProps) {
+  const [teste, useTeste] = useState("");
+
+  return (
+    <li>
+      <strong>{props.repository.name}</strong>
+      <p>{props.repository.description}</p>
+
+      <a target="_blank" href={props.repository.html_url}>
+        Acessar repositório
+      </a>
+    </li>
+  );
+}
